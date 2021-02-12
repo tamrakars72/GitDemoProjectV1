d815a5eecc6af0c70f67328506b76e9fcb22fa15
phxsu <phxsu@DESKTOP-P32CKUU>
2021-02-11 20:30:25 -0700

    My first commit

---
+++ src/Test1.java
@@ -0,0 +1,15 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		String hungry = "yes";
+		
+		if (hungry == "yes") {
+			System.out.println("Eat something....");
+		} else {
+			System.out.println("Do work.....");
+		}
+	}
+
+}
